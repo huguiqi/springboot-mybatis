02e52a6f006bcdb03afcf30fc408c193cebbd8ad
guiqi.hu@163.com <qiguihu198838>
2018-02-05 16:59:45 +0800

    解决oracle自动生成的model有Short类型

--- src/main/java/com/example/demo/common/JavaTypeResolverForOracleImpl.java
+++ src/main/java/com/example/demo/common/JavaTypeResolverForOracleImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.common;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
 
@@ -49,4 +50,22 @@ public class JavaTypeResolverForOracleImpl extends JavaTypeResolverDefaultImpl{
     }
 
 
+    //如果不想用Short作为整型，就重新这个方法
+        @Override
+        protected FullyQualifiedJavaType calculateBigDecimalReplacement(IntrospectedColumn column, FullyQualifiedJavaType defaultType) {
+                FullyQualifiedJavaType answer;
+                if(column.getScale() <= 0 && column.getLength() <= 18 && !this.forceBigDecimals) {
+                        if(column.getLength() > 9) {
+                                answer = new FullyQualifiedJavaType(Long.class.getName());
+                        } else if(column.getLength() > 4) {
+                                answer = new FullyQualifiedJavaType(Integer.class.getName());
+                        } else {
+                                answer = new FullyQualifiedJavaType(Integer.class.getName());
+                        }
+                } else {
+                        answer = defaultType;
+                }
+
+                return answer;
+        }
 }
